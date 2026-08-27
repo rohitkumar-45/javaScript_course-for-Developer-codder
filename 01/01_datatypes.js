018db19f4eece2df8963da9376b37b6947472bf4
ROHIT Yadav. Area of Interest [AI/ML , Data Engineer,  Data Science,  Generative AI  ENGINEER] <115367139+rohitkumar-45@users.noreply.github.com>
2026-08-27 22:32:28 +0000

    datatypes commit done

---
+++ 01/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+
+//alert("Hello") // we are using nodejs, not browser
+
+let name= "rohit"
+let age=14
+let isLoggeddin = false
+
+console.log(name, age, isLoggeddin)
+//bigint
+//string => ""
+//boolean=>true/false
+//null =>standalone CSSMathValue
+// undefined =>
+// symbol => unique
+// number => 2 to power 53
+
+// object
+console.log(typeof "rohit")
+console.log(typeof undefined)
+console.log(typeof null)
+
