3cfcc30334c1a05a913ee6a71a33fc900aa9fb94
ROHIT Yadav. Area of Interest [AI/ML , Data Engineer,  Data Science,  Generative AI  ENGINEER] <115367139+rohitkumar-45@users.noreply.github.com>
2026-08-27 22:07:38 +0000

    commit for variables

---
+++ 01/variables.js
@@ -0,0 +1,9 @@
+const account_Id= 144553
+let account_Email= "rohit@1223"
+var accountpassword= "1234"
+accountcity= "Agra"
+let accountState;
+console.table([account_Id, account_Email, accountpassword, accountcity, accountState])
+
+
+console.log(account_Id);
